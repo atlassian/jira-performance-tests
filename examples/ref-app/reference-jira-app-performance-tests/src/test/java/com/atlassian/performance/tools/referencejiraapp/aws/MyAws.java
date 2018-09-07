c1da4bfedb0f90df37176632191dc05b9c34fd98
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2018-09-07 12:52:03 +0200

    JPERF-48 Remove STSAssumeRoleSessionCredentialsProvider.
    We were using it in our internal CI. It's no longer needed.

--- examples/ref-app/reference-jira-app-performance-tests/src/test/java/com/atlassian/performance/tools/referencejiraapp/aws/MyAws.java
+++ examples/ref-app/reference-jira-app-performance-tests/src/test/java/com/atlassian/performance/tools/referencejiraapp/aws/MyAws.java
@@ -1,30 +1,17 @@
 package com.atlassian.performance.tools.referencejiraapp.aws;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.atlassian.performance.tools.aws.api.Aws;
 import com.atlassian.performance.tools.jiraperformancetests.api.ExplainingAwsCredentialsProvider;
 
-import static java.util.UUID.randomUUID;
-
 public class MyAws {
-
-    /**
-     * Change this to authorize to your AWS account, if the @{@link DefaultAWSCredentialsProviderChain} isn't enough.
-     */
-    private final AWSCredentialsProvider myAwsCredentialsProvider = new STSAssumeRoleSessionCredentialsProvider.Builder(
-        "arn:aws:iam::695067801333:role/JPT",
-        "api-tests-" + randomUUID()
-    ).build();
 
     public final Aws aws = new Aws(
         Regions.US_EAST_1,
         new AWSCredentialsProviderChain(
             new DefaultAWSCredentialsProviderChain(),
-            myAwsCredentialsProvider,
             new ExplainingAwsCredentialsProvider(MyAws.class)
         )
     );
--- src/main/kotlin/com/atlassian/performance/tools/jiraperformancetests/api/ExplainingAwsCredentialsProvider.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraperformancetests/api/ExplainingAwsCredentialsProvider.kt
@@ -2,6 +2,7 @@ package com.atlassian.performance.tools.jiraperformancetests.api
 
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
@@ -13,8 +14,22 @@ class ExplainingAwsCredentialsProvider(
 
     override fun getCredentials(): AWSCredentials {
         """
+        You need to provide AWS credentials to run the test.
+        By default we use ${DefaultAWSCredentialsProviderChain::class.java}.
+
+        AWS credentials provider chain that looks for credentials in this order:
+         * Environment Variables - AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+         * Java System Properties - aws.accessKeyId and aws.secretKey
+         * Credential profiles file at the default location (~/.aws/credentials) shared by all AWS SDKs and the AWS CLI
+         * Credentials delivered through the Amazon EC2 container service if AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"
+          environment variable is set and security manager has permission to access the variable,
+         * Instance profile credentials delivered through the Amazon EC2 metadata service
+
+
+        JPT by default uses Regions.US_EAST_1.
         ****************************************************************
-        You need to set up AWS credentials in $classToCustomize
+        You can also customise AWS credential providers and region in
+        class $classToCustomize.
         ****************************************************************
         """
             .trimIndent()
