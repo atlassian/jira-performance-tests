4f0df312b4f15ff2396ba24a9b8524d9525b0a1e
Sebastian Pawlak <spawlak@atlassian.com>
2018-09-13 12:26:51 +0200

    JPERF-84: Test Jira on premise with custom test scenario

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,10 @@ The API consists of all public Java types from `com.atlassian.performance.tools.
 ## [Unreleased]
 [Unreleased]: https://bitbucket.org/atlassian/jira-performance-tests/branches/compare/master%0Drelease-1.1.0
 
+### Added
+- Add an option to test on premise Jira with custom test scenario which resolves 
+[JPREF-84](https://ecosystem.atlassian.net/browse/JPERF-84)
+
 ## [1.1.0]
 [1.1.0]: https://bitbucket.org/atlassian/jira-performance-tests/branches/compare/release-1.1.0%0Drelease-1.0.0
 
--- src/main/kotlin/com/atlassian/performance/tools/jiraperformancetests/api/OnPremisePerformanceTest.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jiraperformancetests/api/OnPremisePerformanceTest.kt
@@ -1,6 +1,7 @@
 package com.atlassian.performance.tools.jiraperformancetests.api
 
 import com.atlassian.performance.tools.infrastructure.api.virtualusers.LocalVirtualUsers
+import com.atlassian.performance.tools.jiraactions.api.scenario.Scenario
 import com.atlassian.performance.tools.report.api.HistoricalCohortsReporter
 import com.atlassian.performance.tools.virtualusers.api.VirtualUserLoad
 import com.atlassian.performance.tools.virtualusers.api.VirtualUserOptions
@@ -14,30 +15,30 @@ import java.time.Duration
 class OnPremisePerformanceTest(
     private val jira: URI
 ) {
-    var adminLogin: String? = null
-    var adminPassword: String? = null
-    var virtualUsers: Int = 10
+    private companion object {
+        private val DEFAULTS = VirtualUserOptions()
+    }
+
+    var adminLogin: String = DEFAULTS.adminLogin
+    var adminPassword: String = DEFAULTS.adminPassword
+    var scenario: Class<out Scenario> = DEFAULTS.scenario
+    var virtualUsers: Int = DEFAULTS.virtualUserLoad.virtualUsers
     var testDuration: Duration = Duration.ofMinutes(30)
     var workspace: RootWorkspace = RootWorkspace()
     private val testName = "Benchmark my Jira"
     private val cohortName = "my-jira"
 
     fun run() {
-        var options = VirtualUserOptions(
+        val options = VirtualUserOptions(
+            adminLogin = adminLogin,
+            adminPassword = adminPassword,
+            scenario = scenario,
             jiraAddress = jira,
             virtualUserLoad = VirtualUserLoad(
                 virtualUsers = virtualUsers,
                 flat = testDuration
             )
         )
-
-        if (adminLogin != null) {
-            options = options.copy(adminLogin = adminLogin!!)
-        }
-
-        if (adminPassword != null) {
-            options = options.copy(adminPassword = adminPassword!!)
-        }
 
         BtfJiraPerformanceMeter().run(
             virtualUsers = LocalVirtualUsers(workspace = workspace.currentTask.directory),
