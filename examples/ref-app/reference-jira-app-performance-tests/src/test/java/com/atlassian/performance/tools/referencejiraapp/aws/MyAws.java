2fcd4c2c5ca0c8d4ba484c544f0f63dfd3e499bb
Stan Natkovskyi <snatkovskyi@atlassian.com>
2018-09-18 14:44:47 +0200

    JPERF-104: Add comment about unsupported regions

--- examples/ref-app/reference-jira-app-performance-tests/src/test/java/com/atlassian/performance/tools/referencejiraapp/aws/MyAws.java
+++ examples/ref-app/reference-jira-app-performance-tests/src/test/java/com/atlassian/performance/tools/referencejiraapp/aws/MyAws.java
@@ -8,6 +8,9 @@ import com.atlassian.performance.tools.jiraperformancetests.api.ExplainingAwsCre
 
 public class MyAws {
 
+    /**
+     * The following AWS regions are not yet supported: EU_WEST_3, SA_EAST_1, GovCloud
+     */
     public final Aws aws = new Aws(
         Regions.US_EAST_1,
         new AWSCredentialsProviderChain(
